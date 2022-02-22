ef4b847b174f7226c5d59256feda158c0515d5d6
Adrian <48665902+acmatias@users.noreply.github.com>
2022-02-22 12:59:58 +1100

    folio update

---
+++ src/Experience/World/World.js
@@ -0,0 +1,18 @@
+import * as THREE from 'three'
+import Experience from '../Experience.js'
+import Environment from './Environment.js'
+import Floor from './Floor.js'
+
+export default class World {
+    constructor() {
+        this.experience = new Experience()
+        this.scene = this.experience.scene
+        this.resources = this.experience.resources
+
+        this.resources.on('ready', () => {
+            // Setup
+            this.floor = new Floor()
+            this.environment = new Environment()
+        })
+    }
+}
